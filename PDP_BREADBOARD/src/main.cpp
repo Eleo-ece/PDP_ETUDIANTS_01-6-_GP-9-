bface1393101bec1d735b2797105a352b89a0d33
Eleo-ece <rib_ele@outlook.fr>
2023-01-10 11:38:42 +0100

    capteur dht11

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,9 +1,69 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+#define DHTPIN 33     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT11     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+void setup()
+{
+  Serial.begin(9600);
+  // Initialise l'appareil
+  dht.begin();
+  
+  pinMode(LED, OUTPUT);
+
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+
+  // Modifie le délai pour la lecture du capteur
+  delayMS = sensor.min_delay / 5000;
 }
 
-void loop() {
-  // put your code here to run repeatedly
+void loop()
+{
+  
+  // Effectuer une mesure toutes les 5s
+  delay(delayMS);
+  
+  // Afficher la température avec un peu de formatage
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+
+  // Afficher l'humidité relative avec un peu de formatage
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 }
